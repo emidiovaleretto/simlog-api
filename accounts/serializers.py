049a1ad8eb8570fdabf96aac51f4b07256723104
Emidio Valereto Neto <valeretonemidio@meta.com>
2026-05-20 18:57:10 +0100

    feat: implement user authentication with JWT

---
+++ accounts/serializers.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ("simbrief_pilot_id",)
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            password=validated_data["password"],
+        )
+        UserProfile.objects.create(user=user)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "profile")
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop("profile", {})
+        instance.email = validated_data.get("email", instance.email)
+        instance.save()
+
+        profile = instance.profile
+        profile.simbrief_pilot_id = profile_data.get("simbrief_pilot_id", profile.simbrief_pilot_id)
+        profile.save()
+
+        return instance
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('simbrief.urls')),
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("accounts.urls")),
 ]
