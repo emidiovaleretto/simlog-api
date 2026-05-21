5b2028d38eedcb52ae6a1fbeff306d43209b9529
Emidio Valeretto <emidio.valereto@gmail.com>
2026-05-21 13:47:10 +0100

    fix: suppress bandit b104 warning in docker settings

--- core/settings/docker.py
+++ core/settings/docker.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']  # nosec B104
 
 DATABASES = {
     'default': {
