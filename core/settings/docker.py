1b38e10a400ff83edc11edd22472cb192a58e3de
Emidio Valeretto <emidio.valereto@gmail.com>
2026-05-21 13:43:41 +0100

    chore: fix docker setup for macos compatibility

---
+++ core/settings/docker.py
@@ -0,0 +1,18 @@
+from decouple import config
+
+from .base import *
+
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
